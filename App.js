926c12b6958ca689605417906644f00fed628535
Developer <developer@khangaikhuus-MacBook-Pro.local>
2022-10-06 21:35:44 +0800

    test hiisen flattext Component remove hiiv

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import LoginScreen from "./source/screens/loginScreen"
 import Register from './source/screens/registerScreen';
 import FlexboxScreen from './source/screens/flexboxScreen';
 import FlatlistScreen from './source/screens/FlatlistScreen';
-import FlatText from './source/screens/FlatText';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -20,8 +19,7 @@ export default function App() {
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Flexbox" component={FlexboxScreen} />
         <Stack.Screen name="list" component={FlatlistScreen} />
-        <Stack.Screen name="test" component={FlatText} />
-      </Stack.Navigator>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
